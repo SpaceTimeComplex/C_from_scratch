88381644a03df6df9e4d1c359ee36ab5040eeab0
Antonio <104876313+SpaceTimeComplex@users.noreply.github.com>
2026-08-04 11:11:15 +0200

    hello world
    hello, world.

---
+++ hello_world/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* 
+this is 
+a block comment 
+just like C++...or has it always been the otehr way around?
+*/
+
+int main() {
+    printf("Hello World!\n"); // tis a regular comment
+}
